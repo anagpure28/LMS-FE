7f16c56f4f86e1fe94d90380f92de3036cf9359b
Aniket Nagpure <aniketnagpure28@gmail.com>
2024-12-11 01:34:51 +0530

    admin routes, course table, dashboard and sidebar added for admin

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import MainLayout from './layout/MainLayout'
 import Courses from './pages/student/Courses'
 import MyLearning from './pages/student/MyLearning'
 import Profile from './pages/student/Profile'
+import Sidebar from './pages/admin/Sidebar'
+import CourseTable from './pages/admin/course/CourseTable'
+import Dashboard from './pages/admin/Dashboard'
+import AddCourse from './pages/admin/course/AddCourse'
 
 const appRouter = createBrowserRouter([
   {
@@ -35,6 +39,26 @@ const appRouter = createBrowserRouter([
         path: "profile",
         element: <Profile />,
       },
+
+      // Admin routes start from here
+      {
+        path: "admin",
+        element: <Sidebar />,
+        children: [
+          {
+            path: "dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "course",
+            element: <CourseTable />,
+          },
+          {
+            path: "course/create",
+            element: <AddCourse />,
+          },
+        ]
+      }
     ]
   }
 ])
